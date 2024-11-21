9cbf20d6c3160d7b46e26f4387cc9d6eb6d7aaab
SerBy48 <u20221204799@usco.edu.co>
2024-11-21 00:32:37 -0500

    Pagina de Inicio

--- src/Componentes/inicio.jsx
+++ src/Componentes/inicio.jsx
@@ -1,52 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../css/carrusel-novedades.css"
-import polloAsado2 from '../Img/pollo-asado2.jpg';
-import polloAsado3 from '../Img/pollo-asado3.jpg';
+import Productos from './Productos.jsx'; 
+import "../css/novedades.css";
 import polloAsadoB from '../Img/pollo-brocheta.jpg';
-import polloAsadoC from '../Img/pollo-combo.jpg';
 import polloAsadoP from '../Img/pollo-picante.jpg';
-import polloAsadoS from '../Img/pollo-salsas.jpg';
-import polloAsadoV from '../Img/pollo-vino.jpg';
-import polloAsadoBB from '../Img/pollo-bbq.jpg';
-
+import polloAsadoH from '../Img/pollo-hierbas.jpg';
+import polloAsadoR from '../Img/pollo-restaurante.jpg';
+import pollo from '../Img/pollo1.jpg';
 
 function Inicio() {
   return (
     <div>
-      {/* Carrusel */}
+      {/* Bienvenida */}
       <section className="section">
         <div className="container">
           <h2 className="title has-text-centered">¡Bienvenidos a nuestro restaurante!</h2>
-          <div className="carousel">
-            
-            {/* Carrusel de Imágenes */}
-            
-            <div className="carousel-item">
-              <img src={polloAsado2} alt="Pollo asado 2" className="carousel-image" />
+          <div className="columns is-vcentered">
+            <div className="column is-half">
+              <img src={polloAsadoR} alt="Imagen bienvenida" className="img-bienvenida" />
             </div>
-            <div className="carousel-item">
-              <img src={polloAsado3} alt="Pollo asado 3" className="carousel-image" />
+            <div className="column is-half">
+              <p className="texto-bienvenida">
+                En nuestro restaurante, nos enorgullecemos de ofrecer una experiencia culinaria única,
+                donde cada platillo es preparado con ingredientes frescos y de la mejor calidad. ¡Ven y disfruta de
+                nuestra especialidad en pollos asados y mucho más!
+              </p>
             </div>
-          </div>
-
-          {/* Controles del Carrusel */}
-          <div className="carousel-navigation">
-            <button className="button is-primary is-small" aria-label="previous">
-              <span className="icon is-small">
-                <i className="fas fa-chevron-left"></i>
-              </span>
-            </button>
-            <button className="button is-primary is-small" aria-label="next">
-              <span className="icon is-small">
-                <i className="fas fa-chevron-right"></i>
-              </span>
-            </button>
           </div>
         </div>
-
       </section>
-
 
       {/* Sección de Novedades */}
       <section className="section">
@@ -56,76 +38,61 @@ function Inicio() {
           <div className="columns is-multiline">
             {/* Cuadro de Novedad 1 */}
             <div className="column is-4">
+              <Link to="/productos">
               <div className="novedad-box">
                 <img src={polloAsadoB} alt="Pollo Asado en Brocheta" />
                 <div className="content">
                   <h3>Pollo Asado en Brocheta</h3>
-                  <p>Ahora podrás disfrutar de nuestro delicioso pollo asado servido en brocheta. Perfecto para compartir.</p>
-                  <a href="/productos" className="button">Saber más</a>
+                  <p>Ahora podrás disfrutar de nuestro delicioso pollo asado servido en brocheta. Perfecto para compartir con los que más quieres.</p>
                 </div>
               </div>
+              </Link>
             </div>
 
             {/* Cuadro de Novedad 2 */}
             <div className="column is-4">
+              <Link to="/productos">
               <div className="novedad-box">
-                <img src={polloAsadoC} alt="Pollo asado Combo Familiar" />
+                <img src={polloAsadoH} alt="Pollo Asado con Hierbas" />
                 <div className="content">
-                  <h3>Combo Familiar</h3>
-                  <p>Estrenamos el combo familiar, que incluye pollo asado y una selección de guarniciones variadas. ¡Ideal para toda la familia!</p>
-                  <a href="/productos" className="button">Saber más</a>
+                  <h3>Pollo Asado con Hierbas</h3>
+                  <p>Un clásico reinventado con una mezcla de hierbas frescas que aportan un sabor único y delicioso, para aquellos que buscan un sabor más refinado.</p>
                 </div>
               </div>
+              </Link>
             </div>
 
             {/* Cuadro de Novedad 3 */}
             <div className="column is-4">
+              <Link to="/productos">
               <div className="novedad-box">
                 <img src={polloAsadoP} alt="Pollo Asado Picante" />
                 <div className="content">
                   <h3>Pollo Asado Picante</h3>
                   <p>Para los amantes del picante, ahora ofrecemos pollo asado con un toque de salsa picante especial. ¿Te atreves a probarlo?</p>
-                  <a href="/productos" className="button">Saber más</a>
                 </div>
               </div>
+              </Link>
             </div>
+          </div>
+        </div>
+      </section>
 
-            {/* Cuadro de Novedad 4 */}
-            <div className="column is-4">
-              <div className="novedad-box">
-                <img src={polloAsadoS} alt="Pollo Asado con Salsas Especiales" />
-                <div className="content">
-                  <h3>Pollo Asado con Salsas Especiales</h3>
-                  <p>Agregamos una nueva opción a nuestro menú: pollo asado acompañado de deliciosas salsas especiales. ¡No te lo puedes perder!</p>
-                  <a href="/productos" className="button">Saber más</a>
-                </div>
-              </div>
+      {/* Sección Sobre Nosotros */}
+      <section className="section">
+        <div className="container">
+          <h2 className="title has-text-centered">Sobre nosotros</h2>
+          <div className="columns is-vcentered">
+            <div className="column is-half2">
+              <p>
+                Nuestro restaurante ha estado sirviendo a la comunidad durante más de 20 años, ofreciendo
+                platillos que combinan tradición y creatividad. Nos esforzamos por mantener un ambiente acogedor
+                y brindar a nuestros clientes una experiencia memorable en cada visita.
+              </p>
             </div>
-
-            {/* Cuadro de Novedad 5 */}
-            <div className="column is-4">
-              <div className="novedad-box">
-                <img src={polloAsadoV} alt="Pollo Asado al Vino Tinto" />
-                <div className="content">
-                  <h3>Pollo al Vino Tinto</h3>
-                  <p>Una receta especial para los amantes del buen vino. Pollo cocinado a fuego lento con vino tinto y especias, ¡te sorprenderá!</p>
-                  <a href="/productos" className="button">Saber más</a>
-                </div>
-              </div>
+            <div className="column is-half2">
+              <img src={pollo} alt="Imagen de trayectoria"/>
             </div>
-
-            {/* Cuadro de Novedad 6 */}
-            <div className="column is-4">
-              <div className="novedad-box">
-                <img src={polloAsadoBB} alt="Pollo BBQ" />
-                <div className="content">
-                  <h3>Pollo Asado con Salsa BBQ</h3>
-                  <p>¡Prueba nuestra nueva receta de pollo BBQ con un toque especial!</p>
-                  <a href="/productos" className="button">Saber más</a>
-                </div>
-              </div>
-            </div>
-
           </div>
         </div>
       </section>
